f02f203465eeb82ba73206a7bed02b37a3d57a57
kyungwon <kyungwon.jung0@gmail.com>
2018-11-14 16:40:33 +0900

    로또 기능 추가 두번째

--- src/com/company/Lottery.java
+++ src/com/company/Lottery.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -12,7 +14,9 @@ public class Lottery
 
     private Scanner input = new Scanner(System.in);
     private Random randomNumber = new Random();
-    private ArrayList<Integer> lotteryNumber = new ArrayList<>();
+    public ArrayList<Integer> lotteryNumber = new ArrayList<>();
+
+
 
     public Lottery(int minNumber, int maxNumber, int totalCount)
     {
@@ -21,36 +25,127 @@ public class Lottery
         this.TOTAL_COUNT = totalCount;
     }
 
-    public void SelectNumber()
+
+    public void SelectNumber(Boolean showMessage)
     {
         int temp = randomNumber.nextInt(MAX_NUMBER) + MIN_NUMBER;
         if (lotteryNumber.contains(temp))
         {
-            SelectNumber();
+            SelectNumber(showMessage);
         }
         else
         {
             lotteryNumber.add(temp);
-            System.out.printf("숫자 : " + String.valueOf(temp) + "\n");
+            if(showMessage)
+            {
+                System.out.printf("숫자 : " + String.valueOf(temp) + "\n");
+            }
         }
     }
 
-    public void LotteryChoice()
+
+
+    public void LotteryChoice(Boolean showMessage)
     {
         for (int i = 0; i < TOTAL_COUNT; i++)
         {
-            System.out.printf("%d번째 숫자 추첨 (엔터를 입력하세요) :", i + 1);
-            input.nextLine();
-            this.SelectNumber();
+            if (showMessage)
+            {
+                System.out.printf("%d번째 숫자 추첨 (엔터를 입력하세요) :", i + 1);
+                input.nextLine();
+            }
+            this.SelectNumber(showMessage);
         }
     }
 
-    public void ShowLotteryNumber()
+    public void ShowLotteryNumber(Boolean showMessag)
     {
-        System.out.printf("로또 추첨번호 : ");
+        if(showMessag)
+        {
+            System.out.printf("로또 추첨번호 : ");
+        }
+        else
+        {
+            System.out.printf("로또 성공번호 : ");
+        }
+
         for (int i = 0; i < this.lotteryNumber.size() ; i++)
         {
             System.out.printf("%d ", this.lotteryNumber.get(i));
         }
+        System.out.printf("\n");
     }
+
+
+
+
+
+    public int GetGrade(int gameType, ArrayList<Integer> successNumbers, ArrayList<Integer> choiceNumbers )
+    {
+        int sameNumberCount = 0;
+        int grade = 0;
+        for (int i = 0; i < choiceNumbers.size(); i++)
+        {
+            if(successNumbers.contains(choiceNumbers.get(i)))
+            {
+                sameNumberCount += 1;
+            }
+        }
+
+        if (gameType == 1)
+        {
+            grade = 6 - sameNumberCount + 1;
+            if (grade == 7)
+            {
+                return -1;
+            }
+            else
+            {
+                return grade;
+            }
+        }
+        else if (gameType == 2)
+        {
+            if (sameNumberCount == 8)
+            {
+                return 1;
+            }
+            else if(sameNumberCount == 7 || sameNumberCount == 6)
+            {
+                return 2;
+            }
+            else if(sameNumberCount == 5 || sameNumberCount == 4)
+            {
+                return 3;
+            }
+            else if(sameNumberCount == 3 || sameNumberCount == 2)
+            {
+                return 4;
+            }
+            else if(sameNumberCount == 1)
+            {
+                return 5;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        else if (gameType == 3)
+        {
+            if(sameNumberCount == 6)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    return -1;
+    }
+
+
+
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,42 +1,88 @@
 package com.company;
+
 import java.util.Scanner;
+
 
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
         Scanner sc = new Scanner(System.in);
         System.out.printf("Lottery Number\n");
         System.out.printf("로또 1, 2, 3 을 선택 하세요\n\r");
         System.out.printf("로또 종류 숫자 입력 : ");
-
+        int grade ;
         int choiceNumber = sc.nextInt();
+
         if(choiceNumber == 1)
         {
+            /* 로또 성공 번호 생성 */
+            Lottery successNumbers = new Lottery(1,45, 6);
+            successNumbers.LotteryChoice(false);
+
+
+            /* 번호 추첨 */
             Lottery lotto = new Lottery(1,45, 6);
-            lotto.LotteryChoice();
-            lotto.ShowLotteryNumber();
+            lotto.LotteryChoice(true);
+            successNumbers.ShowLotteryNumber(false);
+            lotto.ShowLotteryNumber(true);
+
+            /* 순위 매기기 */
+            grade = lotto.GetGrade(choiceNumber, successNumbers.lotteryNumber, lotto.lotteryNumber);
+            if(grade == -1)
+            {
+                System.out.printf("꽝입니다.\n");
+            }
+            else
+            {
+                System.out.printf("순위 : %d등 당첨 되었습니다.\n", grade);
+            }
         }
         else if(choiceNumber == 2)
         {
+            Lottery successNumbers = new Lottery(1,45, 8);
+            successNumbers.LotteryChoice(false);
+
             Lottery lotto = new Lottery(1,45, 8);
-            lotto.LotteryChoice();
-            lotto.ShowLotteryNumber();
+            lotto.LotteryChoice(true);
+            successNumbers.ShowLotteryNumber(false);
+            lotto.ShowLotteryNumber(true);
+
+            grade = lotto.GetGrade(choiceNumber, successNumbers.lotteryNumber, lotto.lotteryNumber);
+            if(grade == -1)
+            {
+                System.out.printf("꽝입니다.\n");
+            }
+            else
+            {
+                System.out.printf("순위 : %d등 당첨 되었습니다.\n", grade);
+            }
+
         }
         else if(choiceNumber == 3)
         {
-            Lottery lotto = new Lottery(1,80, 6);
-            lotto.LotteryChoice();
-            lotto.ShowLotteryNumber();
-        }
-//        else
-//        {
-//            System.out.printf("숫자 입력이 잘못 되었습니다.");
-//        }
+            Lottery successNumbers = new Lottery(1,80, 6);
+            successNumbers.LotteryChoice(false);
+            successNumbers.ShowLotteryNumber(false);
 
-        //lotto.LotteryChoice();
-        //lotto.ShowLotteryNumber();
+            Lottery lotto = new Lottery(1,80, 6);
+            lotto.LotteryChoice(true);
+            successNumbers.ShowLotteryNumber(false);
+            lotto.ShowLotteryNumber(true);
+            grade = lotto.GetGrade(choiceNumber, successNumbers.lotteryNumber, lotto.lotteryNumber);
+
+            if(grade == -1)
+            {
+                System.out.printf("꽝입니다.\n");
+            }
+            else
+            {
+                System.out.printf("순위 : 1등 당첨 되었습니다.\n");
+            }
+
+        }
+
 
     }
 }
